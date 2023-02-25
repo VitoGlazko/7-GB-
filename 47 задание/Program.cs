6410ed25fccb061a94b65bd8e395bd868fccd370
Glazkov Vitalii <vitalik.glazkov.2014@gmail.com>
2023-02-25 15:49:21 +0600

    Решил задачи 47, 50, 52

---
+++ 47 задание/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+Console.Clear();
+
+double[,] GetArray(int m, int n, double max, double min)
+{
+    double[,] result = new double[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = Math.Round(new Random().NextDouble() * (max-min) + min, 2);
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите кол-во строк: ");
+int str = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбов: ");
+int col = int.Parse(Console.ReadLine()!);
+double[,] array = GetArray(str, col, -20, 20);
+PrintArray(array);
